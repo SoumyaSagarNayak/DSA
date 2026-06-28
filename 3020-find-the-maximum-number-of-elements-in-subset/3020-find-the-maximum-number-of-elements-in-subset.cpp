96ee17e7fb51d9272a3a69eacc1e2cdb04607c22
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-06-28 23:27:53 +0530

    Time: 75 ms (67.66%), Space: 122.7 MB (42.29%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (auto& n : nums)
-            freq[n]++;
 
-        int res = (freq[1] - 1) | 1;
-        freq.erase(1);
+        unordered_map<long long,int> freq;
 
-        for (auto& f : freq) {
-            int n = 0, x = f.first;
+        for(int x : nums)
+            freq[x]++;
 
-            int sq = sqrt(x);
-            if (sq * sq == x && freq.count(sq) && freq[sq] > 1)
-                continue;
+        int ans = 1;
 
-            while (x < 31623 && freq.count(x) && freq[x] > 1) {
-                n += 2;
-                x *= x;
-            }
-
-            res = fmax(res, n + ((freq.count(x) << 1) - 1));
+        // Handle 1 separately
+        if(freq.count(1)){
+            ans = freq[1];
+            if(ans % 2 == 0) ans--;
         }
 
-        return res;
+        for(auto &[num, cnt] : freq){
+
+            if(num == 1) continue;
+
+            long long curr = num;
+            int len = 0;
+
+            while(freq.count(curr)){
+
+                if(freq[curr] >= 2){
+                    len += 2;
+
+                    if(curr > 1000000000LL / curr) break; // avoid overflow
+                    curr *= curr;
+                }
+                else{
+                    len++;
+                    break;
+                }
+            }
+
+            // If we broke due to overflow after adding two,
+            // remove one because the chain cannot continue.
+            if(!freq.count(curr) || (curr > 1000000000LL / max(1LL,curr))){
+                if(len % 2 == 0) len--;
+            }
+
+            ans = max(ans, len);
+        }
+
+        return ans;
     }
 };
