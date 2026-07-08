e7feaaffc5d80acc103caa158dd357d227e65bc2
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-07-08 09:05:44 +0530

    Time: 0 ms (100%), Space: 9 MB (52.06%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        long long x = 0, s = 0;
-        for (char c : to_string(n))
-            if (c != '0')
-                x = x * 10 + c - '0', s += c - '0';
-        return x * s;
+        string nw= to_string(n);
+        long long x=0;
+        int sum=0;
+        for(char ch:nw){
+            if(ch!='0'){
+                x=x*10+(ch-'0');
+                sum += (ch-'0');
+            }
+        }
+        return x*sum;
     }
 };
