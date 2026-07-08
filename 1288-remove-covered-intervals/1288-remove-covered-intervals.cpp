ba137d625432453f42284d9864b70cd40202f2cb
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-07-08 09:22:50 +0530

    Time: 0 ms (100%), Space: 15 MB (98.4%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& A) {
-        ranges::sort(A, {}, [](auto& x) {
-            return pair{x[0], -x[1]};
-        });
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
 
-        int res = 0, r = 0;
+        // Step 1: Sort intervals
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int> &a, vector<int> &b){
 
-        for (auto& x : A) {
-            res += x[1] > r;
-            r = max(r, x[1]);
+                // If starting points are same,
+                // keep larger ending point first.
+                if(a[0] == b[0])
+                    return a[1] > b[1];
+
+                // Otherwise smaller starting point first.
+                return a[0] < b[0];
+             });
+
+        // First interval will always remain.
+        int remaining = 1;
+
+        // Store the largest end seen till now.
+        int maxEnd = intervals[0][1];
+
+        // Check every remaining interval.
+        for(int i = 1; i < intervals.size(); i++){
+
+            // If current interval ends after maxEnd,
+            // it is NOT covered.
+            if(intervals[i][1] > maxEnd){
+
+                remaining++;
+
+                // Update largest ending point.
+                maxEnd = intervals[i][1];
+            }
+
+            // Otherwise it is covered.
+            // So do nothing.
         }
 
-        return res;
+        return remaining;
     }
 };
