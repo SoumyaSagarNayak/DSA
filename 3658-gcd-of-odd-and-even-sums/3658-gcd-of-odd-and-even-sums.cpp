f34340a7a04cefa7bac56fd6858ee765eb899b3b
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-07-15 20:51:54 +0530

    Time: 576 ms (5.15%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,13 @@
+
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int oddsum = n * n;
+        int evensum = n * (n + 1);
+        int gcd = 1;
+        for (int i = 1; i <= min(oddsum, evensum); i++) {
+            if (oddsum % i == 0 && evensum % i == 0) gcd = i;
+        }
+        return gcd;
     }
 };
