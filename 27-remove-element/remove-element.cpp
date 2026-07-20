3c7f4b46755acd7ba9d82991ce1eb08159bfeabd
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-07-20 14:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (12.62%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int num=0;
-        int k=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val)
-            {
-                nums[k]=nums[i];
-                k++;
-            }
-            
+
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != val)
+                ans.push_back(nums[i]);
         }
-        return k;
+
+        nums = ans;
+
+        return nums.size();
     }
 };
+
+// or
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+
+//         int k = 0;
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] != val)
+//             {
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+
+//         return k;
+//     }
+// };
