0c4e8a86c846f416e5ecd2650fec9311d8087629
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-07-23 21:42:59 +0530

    Time: 0 ms (100%), Space: 25.1 MB (5.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -11,3 +11,27 @@ public:
         nums = arr;
     }
 };
+
+// or
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+
+//         int k = 0;
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] != 0)
+//             {
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+
+//         while(k < nums.size())
+//         {
+//             nums[k] = 0;
+//             k++;
+//         }
+//     }
+// };
