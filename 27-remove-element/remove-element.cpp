8785ed248cbafcdb2213b646d83ac078b8073a63
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-08-03 15:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.32%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-
-        vector<int> ans;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] != val)
-                ans.push_back(nums[i]);
+        int n = nums.size() ;
+        int left = 0 , right = n - 1 ;
+        while(left <= right) {
+     if (nums[left] == val) {
+                swap(nums[left], nums[right]);
+                right--;
+            }
+            else {
+                left++;
+            }
         }
-
-        nums = ans;
-
-        return nums.size();
+        return left ;
     }
 };
-
-// or
-// class Solution {
-// public:
-//     int removeElement(vector<int>& nums, int val) {
-
-//         int k = 0;
-
-//         for(int i = 0; i < nums.size(); i++)
-//         {
-//             if(nums[i] != val)
-//             {
-//                 nums[k] = nums[i];
-//                 k++;
-//             }
-//         }
-
-//         return k;
-//     }
-// };
