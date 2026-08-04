5649fcaf2fe76ede81bedaaead06ab0f41acacdb
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-08-04 15:40:21 +0530

    Time: 0 ms (100%), Space: 32.4 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -7,10 +7,6 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
-        int small = nums[0];
-        int large = nums[n - 1];
-
-     
         for (int i = 0; i < n - 1; i++) {
             if (nums[i + 1] - nums[i] > 1) {
                 for (int j = nums[i] + 1; j < nums[i + 1]; j++)
