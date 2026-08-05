aec7706aea898b9865d14b39864c912237d66bad
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-08-06 01:15:50 +0530

    Time: 67 ms (19.91%) | Memory: 28.5 MB (75.27%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+        if not prices:
+            return 0
+            
         minn=prices[0]
         maxx=0
         curr=0
