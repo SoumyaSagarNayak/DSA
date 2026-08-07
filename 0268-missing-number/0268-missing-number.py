49f0a55af118ce8a026a364a14c5fb53099a03e9
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-08-07 16:33:52 +0530

    Time: 0 ms (100%), Space: 20.3 MB (74.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for index,value in enumerate(nums):
-            if(index!=value):
-                return value-1
-            if value==len(nums)-1:
-                return value+1
+        return sum(range(len(nums)+1))-sum(nums)
+
         
