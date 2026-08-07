0e8a6ee7b824e25cf2e7887d6fad419d77781648
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-08-07 17:02:26 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary={}
+        map={}
         for i in range(len(nums)):
             req=target-nums[i]
 
-            if req in dictionary:
-                return[dictionary[req],i]
-
-            dictionary[nums[i]]=i
+            if req in map:
+                return [map[req],i]
+            map[nums[i]]=i
         
