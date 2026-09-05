38797519f001e4d93d3ab7d7607bfd99bf618ee0
Soumya Sagar Nayak <158138493+SoumyaSagarNayak@users.noreply.github.com>
2026-09-05 15:50:44 +0530

    Time: 12 ms (17.94%) | Memory: 19.3 MB (70.81%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -1,18 +1,25 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-
-
+        maxx=float('-inf')
+        maxx=float('-inf')
         
+       
         for i in range(len(nums)):
             minn=float('inf')
-            maxx=float('-inf')
-            for j in range(0,i+1):
-                maxx=max(maxx,nums[j])
-                
+            nminn=float('inf')
+
+            
+            
+            maxx=max(maxx,nums[i])
+
             for j in range(i,len(nums)):
                 minn=min(minn,nums[j])
+                
+                
 
             diff=maxx-minn
+            
+
 
             if diff<=k:
                 return i
